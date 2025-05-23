00e47db401019bd3b4c3abbd8c88490d0693f52f
RABI'ATUL'ADAWIYAH BINTI ABDUL RAHMAN <rabek.man@gmail.com>
2025-05-23 12:52:04 +0800

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+greeting = 'Hello, World!'
+print(greeting)
+# This is a simple Python program that prints "Hello, World!" to the console.
+# It demonstrates the basic syntax of Python and how to use the print function
+
+# to display output.
+# How to run this code:
+# 1. Save the code in a file named HelloWorld.py.
+# 2. Open a terminal or command prompt.
+# 3. Navigate to the directory where the file is saved.
+# 4. Run the command: python HelloWorld.py
+# 5. You should see "Hello, World!" printed in the console.
